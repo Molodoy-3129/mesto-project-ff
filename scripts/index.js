643a8f37c31a9677c52d065d649599c17d5c723a
molodoy <firegon3129@gmail.com>
2024-07-05 18:58:01 +0300

    ddfff

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,40 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+
+
+const cardList = document.querySelector('.places__list');
+const template = document.querySelector('#card-template').content;
+
+function deleteCard(card) {
+     card.remove();
+}
+
+
+
+
+
+function createCard(name, link, deleteCard) {
+     const cardElement = template.querySelector('.card').cloneNode(true);
+     const cardImage = cardElement.querySelector('.card__image');
+     cardImage.setAttribute('src', link);
+     cardImage.setAttribute('alt', `Картинка места "${name}"`);
+     cardElement.querySelector('.card__title').textContent = name;
+
+     const deletButton = cardElement.querySelector('.card__delete-button');
+     deletButton.addEventListener('click', function() {
+          deleteCard(cardElement);
+     })
+  
+    
+     return cardElement;
+
+}
+
+initialCards.forEach((item) => {
+
+     const newCard = createCard(item.name, item.link, deleteCard);
+     cardList.append(newCard);
+})
